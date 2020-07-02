6e6957571dbe1fbabb0dacc9c2616e4289e1cbde
Vabhav Patel <vpatelsj@gmail.com>
2020-07-01 17:45:48 -0700

    adding a diagnostics checks

--- cmd/collector.go
+++ cmd/collector.go
@@ -1,9 +1,21 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/magefile/mage/sh"
 	"github.com/spf13/cobra"
+	"os"
 )
+type DiagInfo struct {
+	HostName string
+	DockerDaemon string
+	ChronyDaemon string
+	KubeletDameon string
+	AzSecPack string
+	HostInfo string
+	InternetReachable bool
+}
 
 func CollectDiagnosticsCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -12,9 +24,42 @@ func CollectDiagnosticsCommand() *cobra.Command {
 		Long: "Use the collect command to gather bunch of node diagnostic information",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("Collecting node information")
+			di := DiagInfo{
+				HostName: getHostName(),
+				DockerDaemon:      checkDaemonStatus("docker"),
+				ChronyDaemon:      checkDaemonStatus("chrony"),
+				KubeletDameon:     checkDaemonStatus("kubelet"),
+				AzSecPack:         checkDaemonStatus("azsecpack"),
+				HostInfo:          checkDaemonStatus("hostinfo"),
+				InternetReachable: false,
+			}
+
+			b, err := json.Marshal(di);
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(b))
 			return nil
 		},
 	}
 
 	return cmd
+}
+
+
+func checkDaemonStatus(name string) string {
+	if err := sh.Run("systemctl", "is-active","--quiet", "service", name); err != nil {
+		fmt.Println(err)
+		return "NotRunning"
+	}
+	return "Running"
+}
+
+func getHostName() string {
+	name, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return name
 }
